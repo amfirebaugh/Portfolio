e2a4071c1630e308fd7e6b9e5b08c88772aba356
Phoenicis <alliefirebaugh@gmail.com>
2019-01-27 14:47:32 -0500

    fixed nav bar issues and Scrollspy

--- assets/app.js
+++ assets/app.js
@@ -2,14 +2,22 @@ $(document).ready(function(){
     $('.parallax').parallax();
 });
 
-$("#projectLink").on("click", function() {
-    $([document.documentElement, document.body]).animate({
-        scrollTop: $("#alliePortfolio").offset().top
-    }, 1300);
+// $("#projectLink").on("click", function() {
+//     $([document.documentElement, document.body]).animate({
+//         scrollTop: $("#alliePortfolio").offset().top
+//     }, 1300);
+// });
+
+// $("#contactLink").on("click", function() {
+//     $([document.documentElement, document.body]).animate({
+//         scrollTop: $("#allieContact").offset().top
+//     }, 1300);
+// });
+
+$(document).ready(function(){
+    $('.scrollspy').scrollSpy();
 });
 
-$("#contactLink").on("click", function() {
-    $([document.documentElement, document.body]).animate({
-        scrollTop: $("#allieContact").offset().top
-    }, 1300);
-});
+$("#navProjectLink").on("click", function(alliePortfolio) {
+    return 'a[href="' + alliePortfolio +'"]';
+})
