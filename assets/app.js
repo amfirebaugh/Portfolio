1e1790b8403a2731349b4ad76f6d7ad8d733d552
Phoenicis <alliefirebaugh@gmail.com>
2019-01-26 22:28:05 -0500

    continuing updates

--- assets/app.js
+++ assets/app.js
@@ -1,3 +1,15 @@
 $(document).ready(function(){
     $('.parallax').parallax();
 });
+
+$("#projectLink").on("click", function() {
+    $([document.documentElement, document.body]).animate({
+        scrollTop: $("#alliePortfolio").offset().top
+    }, 1300);
+});
+
+$("#contactLink").on("click", function() {
+    $([document.documentElement, document.body]).animate({
+        scrollTop: $("#allieContact").offset().top
+    }, 1300);
+});
