fe22972877769810fd3dc02ef884811f7f8b984b
Phoenicis <alliefirebaugh@gmail.com>
2019-03-23 09:35:44 -0400

    updates to nav bar, overall structure for mobile responsiveness, and icon labels in project section

--- assets/app.js
+++ assets/app.js
@@ -2,3 +2,8 @@
 $(document).ready(function(){
     $('.scrollspy').scrollSpy();
 });
+
+// this initiates hamburger icon on small screens for links on right in navbar
+$(document).ready(function(){
+    $('.sidenav').sidenav();
+});
